b2f2ed6d7bef0c4a2aab832fe5f59116c1236092
saimonemon46 <saimonemon46@gmail.com>
2026-01-03 22:56:58 +0600

    User Features Model & Views

---
+++ Models/SeatSelectionViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Air.Models
+{
+    public class SeatSelectionViewModel
+    {
+        public int FlightId { get; set; }
+        public string FlightName { get; set; }
+        public decimal TicketPrice { get; set; }
+        public int SeatRows { get; set; }
+        public int SeatCols { get; set; }
+
+        // List of booked seat labels (e.g. "1A", "2B")
+        public List<string> BookedSeats { get; set; } = new List<string>();
+
+        // ✅ Helper properties
+        public int TotalSeats => SeatRows * SeatCols;
+
+        public int AvailableSeats => TotalSeats - (BookedSeats?.Count ?? 0);
+    }
+}
