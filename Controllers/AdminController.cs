8626a4fb565e86b90f027cf04911e3c3a9b5dca6
saimonemon46 <saimonemon46@gmail.com>
2026-01-03 22:42:26 +0600

    Admin Dashboard

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Air.Data;
+using Air.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Air.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Admin/Dashboard
+        public IActionResult Dashboard()
+        {
+            var email = HttpContext.Session.GetString("UserEmail");
+            if (email == null || email.ToLower() != "saimonemon46@gmail.com")
+                return RedirectToAction("Login", "Account");
+
+            return View("AdminDashboard");
+        }
+
+        
+    }
+}
